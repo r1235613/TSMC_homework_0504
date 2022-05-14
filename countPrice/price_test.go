162704af55d2dd19c8f76792d66c2f8f8323f124
r1235613 <r123613@gmail.com>
2022-05-14 22:58:53 +0800

    完成簡單折扣範例

--- countPrice/price_test.go
+++ countPrice/price_test.go
@@ -22,3 +22,18 @@ func TestPrice(t *testing.T) {
 		t.Errorf("價格錯誤: %f", value)
 	}
 }
+
+func TestPriceDiscounts(t *testing.T) {
+	if value := price([]int{0, 1}); value != 8.0*2*0.95 {
+		t.Errorf("價格錯誤: %f", value)
+	}
+	if value := price([]int{0, 2, 4}); value != 8.0*3*0.9 {
+		t.Errorf("價格錯誤: %f", value)
+	}
+	if value := price([]int{0, 1, 2, 4}); value != 8.0*4*0.8 {
+		t.Errorf("價格錯誤: %f", value)
+	}
+	if value := price([]int{0, 1, 2, 3, 4}); value != 8.0*5*0.75 {
+		t.Errorf("價格錯誤: %f", value)
+	}
+}
