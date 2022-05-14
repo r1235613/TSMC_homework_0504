a5d5b7dca492ef28a153f530a231ef8320561322
r1235613 <r123613@gmail.com>
2022-05-14 22:55:40 +0800

    完成基礎的價格計算

--- countPrice/price_test.go
+++ countPrice/price_test.go
@@ -3,5 +3,22 @@ package countPrice
 import "testing"
 
 func TestPrice(t *testing.T) {
-
+	if value := price([]int{}); value != 0.0 {
+		t.Errorf("價格錯誤: %f", value)
+	}
+	if value := price([]int{1}); value != 8.0 {
+		t.Errorf("價格錯誤: %f", value)
+	}
+	if value := price([]int{2}); value != 8.0 {
+		t.Errorf("價格錯誤: %f", value)
+	}
+	if value := price([]int{3}); value != 8.0 {
+		t.Errorf("價格錯誤: %f", value)
+	}
+	if value := price([]int{4}); value != 8.0 {
+		t.Errorf("價格錯誤: %f", value)
+	}
+	if value := price([]int{1, 1, 1}); value != 8.0*3 {
+		t.Errorf("價格錯誤: %f", value)
+	}
 }
