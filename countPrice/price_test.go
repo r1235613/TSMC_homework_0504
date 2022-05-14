ae8ec4c1b88829b3d22884d9ab93fccf79bda4b0
r1235613 <r123613@gmail.com>
2022-05-14 23:13:22 +0800

    優化錯誤顯示

--- countPrice/price_test.go
+++ countPrice/price_test.go
@@ -4,22 +4,22 @@ import "testing"
 
 func TestPrice(t *testing.T) {
 	if value := price([]int{}); value != 0.0 {
-		t.Errorf("價格錯誤: %f，正確應為0元", value)
+		t.Errorf("價格錯誤: %f，正確應為%f元", value, 8.0)
 	}
 	if value := price([]int{1}); value != 8.0 {
-		t.Errorf("價格錯誤: %f，正確應為0元", value)
+		t.Errorf("價格錯誤: %f，正確應為%f元", value, 8.0)
 	}
 	if value := price([]int{2}); value != 8.0 {
-		t.Errorf("價格錯誤: %f，正確應為0元", value)
+		t.Errorf("價格錯誤: %f，正確應為%f元", value, 8.0)
 	}
 	if value := price([]int{3}); value != 8.0 {
-		t.Errorf("價格錯誤: %f，正確應為0元", value)
+		t.Errorf("價格錯誤: %f，正確應為%f元", value, 8.0)
 	}
 	if value := price([]int{4}); value != 8.0 {
-		t.Errorf("價格錯誤: %f，正確應為0元", value)
+		t.Errorf("價格錯誤: %f，正確應為%f元", value, 8.0)
 	}
 	if value := price([]int{1, 1, 1}); value != 8.0*3 {
-		t.Errorf("價格錯誤: %f，正確應為0元", value)
+		t.Errorf("價格錯誤: %f，正確應為%f元", value, 8.0*3)
 	}
 }
 
